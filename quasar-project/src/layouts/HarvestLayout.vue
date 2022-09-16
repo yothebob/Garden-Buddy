661b396f4481d54a4e9e7fe927fa6f8f50b9c123
Brandon Brodrick <bbrodrick@sysresccd.internal>
2022-09-16 16:54:14 -0700

    working on harvest page

--- quasar-project/src/layouts/HarvestLayout.vue
+++ quasar-project/src/layouts/HarvestLayout.vue
@@ -39,59 +39,119 @@
     </q-drawer>
 
     <q-page-container>
-      <router-view />
+	<router-view />
+	
+	<div v-for="item in _harvestedPlants">
+	    <div>
+		<input name="userplant" type="text" value=""/>
+		<input name="plant" type="text" value=""/>
+		<input name="quantity" type="text" value=""/>
+		<input name="pound" type="text" value=""/>
+		<input name="ounce" type="text" value=""/>
+		<input name="notes" type="text" value=""/>
+		<input name="metadata" type="text" value=""/>
+	    </div>    
+	</div>
+	<div>
+	    <button @click="AnotherHarvestedPlant">
+		Track Another Plant
+	    </button>
+	</div>
     </q-page-container>
   </q-layout>
 </template>
 
 <script>
-import { defineComponent, ref } from 'vue'
-import EssentialLink from 'components/EssentialLink.vue'
+ import { defineComponent, ref } from 'vue'
+ import EssentialLink from 'components/EssentialLink.vue'
+ import axios from 'axios'
+ 
+ const linksList = [
+     {
+	 title: 'Home',
+	 caption: 'My Home Page',
+	 icon: 'school',
+	 link: '/home/'
+     },
+     {
+	 title: 'Gardens',
+	 caption: 'View, create and update my gardens',
+	 icon: 'school',
+	 link: '/gardens/'
+     },
+     {
+	 title: 'My Plants',
+	 caption: 'Track any data you want with custom plants!',
+	 icon: 'code',
+	 link: '/userplants/'
+     },
+     {
+	 title: 'Harvests',
+	 caption: 'Add a new harvest for the day',
+	 icon: 'chat',
+	 link: '/harvests/'
+     },
+ ]
 
-const linksList = [
-  {
-      title: 'Home',
-      caption: 'My Home Page',
-      icon: 'school',
-      link: '/home/'
-  },
-  {
-      title: 'Gardens',
-      caption: 'View, create and update my gardens',
-      icon: 'school',
-      link: '/gardens/'
-  },
-  {
-      title: 'My Plants',
-      caption: 'Track any data you want with custom plants!',
-      icon: 'code',
-      link: '/userplants/'
-  },
-  {
-      title: 'Harvests',
-      caption: 'Add a new harvest for the day',
-      icon: 'chat',
-      link: '/harvests/'
-  },
-]
-
-export default defineComponent({
-  name: 'HarvestLayout',
-
-  components: {
-    EssentialLink
-  },
-
-  setup () {
-    const leftDrawerOpen = ref(false)
-
-    return {
-      essentialLinks: linksList,
-      leftDrawerOpen,
-      toggleLeftDrawer () {
-        leftDrawerOpen.value = !leftDrawerOpen.value
-      }
-    }
-  }
-})
+ export default defineComponent({
+     name: 'HarvestLayout',
+     data: () => {
+	 return {
+	     "_harvestedPlants": null,
+	 };
+     },
+     components: {
+	 EssentialLink
+     },
+     setup () {
+	 const leftDrawerOpen = ref(false)
+	 
+	 return {
+	     essentialLinks: linksList,
+	     leftDrawerOpen,
+	     toggleLeftDrawer () {
+		 leftDrawerOpen.value = !leftDrawerOpen.value
+	     }
+	 }
+     },
+     methods: {
+	 AnotherHarvestedPlant: function () {
+	     this._harvestedPlants.push({
+		 "userplant_id": null,
+		 "plant_id": null,
+		 "quantity": null,
+		 "pound": null,
+		 "ounce": null,
+		 "notes": null,
+		 "metadata": null
+	     })
+	 },
+	 apiGetPlantData: function () {
+	     axios.get("/api/plants/").then((response) => {
+		 console.log(response);
+	     })
+	 },
+	 apiGetVarietyData: function (given_plant_id) {
+	     axios.get(`/api/varietys/plant_id=${given_plant_id}`).then((response) => {
+		 console.log(response);
+	     })
+	 },
+	 apiGetUserData: function () {
+	     /* TODO add jwt */
+	     let user_id = 1;
+	     // I think I can make an api call that checks the backend for user_id and passes that up
+	     axios.get(`/api/user/user_id=${user_id}`).then((response) => {
+		 console.log(response);
+	     })
+	 },
+	 printme: function () {
+	     /* TODO add jwt */
+	     axios.post(`/api/login/?username=${this._username}&password=${this._password}`, {}).then((response) => {
+		 if (response.status === 200) {
+		     window.location.href = '/home/'
+		 }
+	     })
+	 }
+     }
+ })
 </script>
